fd93369f0995b249881e41aa0b7c9f0228846f60
Ryan <ryweller@gmail.com>
2024-07-07 22:31:32 -0500

    chore: reverted to fix git BFG issues and include recent fixes to AI, data_processing, main, and ignore: Improve data processing and peak ROR calculation

--- src/AI.py
+++ src/AI.py
@@ -1,18 +1,19 @@
 # %%
 # This script uses GPT-3.5 to identify the country of origin from the roastName
 import os
-import openai
 from openai import OpenAI
-import pandas as pd
 import json
-import time
 #using the OAI key ending 'dm0k2'
 
-def get_origin(roast_name):
-  client = OpenAI()
 
+## Set the API key and model name
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+#OAI_model = "gpt-3.5-turbo" # currently stuck on this model under a the Free Tier until I spend more $$
+OAI_model = "gpt-4o" 
+def get_origin(roast_name):
   completion = client.chat.completions.create(
-      model="gpt-3.5-turbo-1106",
+      model=OAI_model,
       response_format={ "type": "json_object" },
       messages=[
         {
@@ -31,7 +32,10 @@ def get_origin(roast_name):
       seed=42, #using seed to get consistent results but this takes a lot longer to run (2.5 secs with vs 0.8 w/out)
       max_tokens=70, # Using <20 certainly threw errors, also a few at 30 
       temperature=0.5,
-  )
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
   
   # .loads converts string to dict, .get returns value of key "Origin" or None if not present
   originName = json.loads(completion.choices[0].message.content).get("Origin", None) 
